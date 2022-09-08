ac01a21fac9e2ebaa0f662b1cc58df9ce57e788a
DarmisonFonteles <darmisonaugusto@gmail.com>
2022-09-08 16:17:30 -0300

    moduficações e responsividade

--- projeto-connvert/assent/script.js
+++ projeto-connvert/assent/script.js
@@ -1,17 +1,19 @@
-const divs = document.getElementById("sobre");
-const div = document.querySelectorAll("#sobre sobre");
-$('#sobre');
-
-
-let sm = 0;
-
-function carrossel(){
-    sm++;
-
-    if(sm > divs.length - 1){
-        sm = 0;
-    }
-    divs.style.transform = `translateX($(-sm * 500)px)`;
-}
-
-setInterval(carrossel, 1800);
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        dots: false,
+        responsive:{
+            0:{
+                item:1
+            },
+            600:{
+                item:1
+            },
+            1200:{
+                item:1
+            }
+        },
+        autoplay: true,
+        autoplayTimeout: 5000,
+    });
+})
